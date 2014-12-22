21430b1b0fb18d6ee493e9260cac6ae258aca8f2
Garrett Berg <gberg@neutral.io>
2014-12-22 16:18:34 -0700

    BLK-226 fix patching. still unrelated error

--- tests/test_twitter_post_block.py
+++ tests/test_twitter_post_block.py
@@ -6,9 +6,8 @@ from nio.util.support.block_test_case import NIOBlockTestCase
 
 
 class TestTwitterPost(NIOBlockTestCase):
-
-    @patch('twitter_rest.twitter_post_block.TwitterPost._authorize')
-    @patch('twitter_rest.twitter_post_block.TwitterPost._post_tweet')
+    @patch(TwitterPost, '_authorize')
+    @patch(TwitterPost, '_post_tweet')
     def test_process_post(self, mock_post, mock_auth):
         signals = [Signal({'foo': 'test signal'})]
         blk = TwitterPost()
@@ -22,8 +21,8 @@ class TestTwitterPost(NIOBlockTestCase):
         mock_post.assert_called_once_with({'status': signals[0].foo})
         blk.stop()
 
-    @patch('twitter_rest.twitter_post_block.TwitterPost._authorize')
-    @patch('twitter_rest.twitter_post_block.TwitterPost._post_tweet')
+    @patch(TwitterPost, '_authorize')
+    @patch(TwitterPost, '_post_tweet')
     def test_process_multiple(self, mock_post, mock_auth):
         signals = [
             Signal({'foo': 'test signal'}),
