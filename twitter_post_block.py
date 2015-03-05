2acb9367784eb2b54b51e5b6266403434275b392
mattdodge <matt@n.io>
2015-03-04 16:22:16 -0800

    Using base block in twitter post - to get default values in metadata - fixes #4

--- twitter_post_block.py
+++ twitter_post_block.py
@@ -1,16 +1,8 @@
 import requests
-import json
-from urllib.parse import quote
-import oauth2 as oauth
-from requests_oauthlib import OAuth1
 
-from nio.common.block.base import Block
+from .twitter_rest_base_block import TwitterRestBase
 from nio.common.discovery import Discoverable, DiscoverableType
-from nio.metadata.properties.object import ObjectProperty
-from nio.metadata.properties.holder import PropertyHolder
-from nio.metadata.properties.expression import ExpressionProperty
-from nio.metadata.properties.string import StringProperty
-from nio.modules.threading import Thread
+from nio.metadata.properties import ExpressionProperty
 
 
 VERIFY_CREDS_URL = ('https://api.twitter.com/1.1/'
@@ -18,30 +10,10 @@ VERIFY_CREDS_URL = ('https://api.twitter.com/1.1/'
 POST_URL = "https://api.twitter.com/1.1/statuses/update.json"
 
 
-class TwitterCreds(PropertyHolder):
-
-    """ Property holder for Twitter OAuth credentials.
-
-    """
-    consumer_key = StringProperty(title='Consumer Key')
-    app_secret = StringProperty(title='App Secret')
-    oauth_token = StringProperty(title='OAuth Token')
-    oauth_token_secret = StringProperty(title='OAuth Token Secret')
-
-
 @Discoverable(DiscoverableType.block)
-class TwitterPost(Block):
+class TwitterPost(TwitterRestBase):
 
     status = ExpressionProperty(default='', title='Status Update')
-    creds = ObjectProperty(TwitterCreds, title='Credentials')
-
-    def __init__(self):
-        super().__init__()
-        self._auth = None
-
-    def start(self):
-        super().start()
-        self._auth = self._authorize()
 
     def process_signals(self, signals):
         for s in signals:
@@ -78,21 +50,3 @@ class TwitterPost(Block):
             self._logger.debug(
                 "Posted '{0}' to Twitter!".format(payload['status'])
             )
-
-    def _authorize(self):
-        """ Prepare the OAuth handshake and verify.
-
-        """
-        try:
-            auth = OAuth1(self.creds.consumer_key,
-                          self.creds.app_secret,
-                          self.creds.oauth_token,
-                          self.creds.oauth_token_secret)
-            resp = requests.get(VERIFY_CREDS_URL, auth=auth)
-            if resp.status_code != 200:
-                raise Exception("Status %s" % resp.status_code)
-            return auth
-        except Exception:
-            self._logger.error("Authentication Failed "
-                               "for consumer key: %s" %
-                               self.creds.consumer_key)
